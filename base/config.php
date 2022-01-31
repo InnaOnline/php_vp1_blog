cfb0c4908795ad08408632b8d38bea8eed91b401
unknown <https://github.com/InnaOnline/todo.git>
2022-01-31 11:58:09 +0300

    vp1

---
+++ base/config.php
@@ -0,0 +1,13 @@
+<?php
+const DB_USER = 'root';
+const DB_NAME = 'loftschools';
+const DB_HOST = 'localhost';
+const DB_PASSWORD = 'loftschools';
+
+const ADMIN_IDS = [9];
+
+function d(...$args)
+{
+    var_dump($args);
+    die;
+}
